2a69906373ce5185330e1611096e80675e1b5818
Bruno Guimarães <brunoguimaraes@gmail.com>
2015-07-20 19:24:00 -0300

    hello world 2

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "Hello World";
+        echo "Hello World 2";
         ?>
     </body>
 </html>
